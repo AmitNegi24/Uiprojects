6dde12a8ce4b39515f6c8800545371a5f109a41c
Amit Negi <mramitnegig1999@gmail.com>
2022-11-15 20:35:13 +0530

    second commit

--- Student UI/studentapp/src/Components/studentrecord/AddStudent.js
+++ Student UI/studentapp/src/Components/studentrecord/AddStudent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import './AddStudent.css';
 import { useNavigate} from 'react-router-dom';
 
@@ -11,40 +11,29 @@ function AddStudent() {
         navigate('/Srecords');
     };
 
-    const [student, setStudent] = useState({
-        Name: "", Age: "", MobileNumber: "", Email: ""
-    });
-    let name, value;
+        const [Name, setName] = useState('');
+        const [Age, setAge] = useState('');
+        const [Mobile, setMobile] = useState('');
+        const [Email, setEmail] = useState('');
 
-    const handleInputs = (e) => {
-        console.log(e);
-        name = e.target.name;
-        value = e.target.value;
-
-        setStudent({ ...student, [name]: value })
-    }
-
-    const PostData = async(e)=>{
-        
-        e.preventDefault();
-
-        const { Name, Age, MobileNumber, Email}=student;
-
-       const res=await fetch("/", {
-        method: "POST",
-        body: JSON.stringify({
-            Name, Age, MobileNumber, Email
-        })
-       });
-       const data =await res.json();
-       if(data.status === 422 || !data){
-        Window.alert("INVALID ENTRY");
-       }
-       else{
-        Window.alert("NEW DATA ENTERED");
-       }
-    }
-
+        function SaveUser()
+        {
+            console.warn({Name,Age,Mobile,Email});
+            let data={Name,Age,Mobile,Email}
+            fetch('http://localhost:3001/create',{
+                method:'POST',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(data)
+            }).then((result)=>{
+                console.warn("result",result)
+                result.json().then((resp)=>{
+                    console.warn("resp",resp)
+                })
+                })
+        }
 
 
 
@@ -52,41 +41,40 @@ function AddStudent() {
         <div>
             <div className='Container'>
                 <h1>AddStudent</h1>
-                <form method='POST'>
+                <form>
                     <div class="form-group row">
                         <label for="inputName" class="col-sm-2 col-form-label">Name</label>
                         <div class="col-sm-5">
-                            <input type="text" class="form-control" name='Name' id="inputName" placeholder="Name"
-                                value={student.Name}
-                                onChange={handleInputs}
+                            <input type="text" class="form-control" name='Name' id="inputName" placeholder="Name" 
+                            value={Name}
+                            onChange={(e) => setName(e.target.value)}
                             />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="inputPassword3" class="col-sm-2 col-form-label">Age</label>
                         <div class="col-sm-5">
-                            <input type="number" class="form-control" name='Age' id="inputAge" placeholder="Age" min="1" max="100"
-                                value={student.Age}
-                                onChange={handleInputs}
-
+                            <input type="number" class="form-control" name='Age' id="inputAge" placeholder="Age" min="1" max="100" 
+                            value={Age}
+                            onChange={(e) => setAge(e.target.value)}
                             />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="inputPassword3" class="col-sm-2 col-form-label">Mobile</label>
                         <div class="col-sm-5">
-                            <input type="tel" class="form-control" name='MobileNumber' id="inputPassword3" placeholder="Mobile number"
-                                value={student.MobileNumber}
-                                onChange={handleInputs}
+                            <input type="tel" class="form-control" name='MobileNumber' id="inputPassword3" placeholder="Mobile number" 
+                            value={Mobile} 
+                            onChange={(e) => setMobile(e.target.value)}
                             />
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-5">
-                            <input type="email" class="form-control" name='Email' id="inputEmail3" placeholder="Email"
-                                value={student.Email}
-                                onChange={handleInputs}
+                            <input type="email" class="form-control" name='Email' id="inputEmail3" placeholder="Email" 
+                            value={Email}
+                            onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                     </div>
@@ -94,7 +82,7 @@ function AddStudent() {
 
                     <div class="form-group row">
 
-                        <button id="button" type="submit" onClick={PostData}>Add Student</button>
+                        <button id="button" type="submit" onClick={SaveUser}>Add Student</button>
                         <button id="button" type="submit" onClick={navigateToSrecords}>cancel</button>
 
                     </div>
